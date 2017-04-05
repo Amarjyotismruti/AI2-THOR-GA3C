68b676e3bca7b10fa6bad0ed9d72b9a4623aa5b3
George Tan <georget1@andrew.cmu.edu>
2017-04-05 05:04:04 -0400

    Add A3C model for cartpole

--- a3c_model.py
+++ a3c_model.py
@@ -6,6 +6,8 @@ from keras.models import Model
 def build_policy_and_value_networks(model_name, num_actions, input_shape, window):
     if 'a3c_networks' in model_name:
         return build_a3c_networks(num_actions, window, input_shape)
+    elif 'cartole' in model_name:
+        return build_cartpole_networks(num_actions, window, input_shape)
 
 def build_a3c_networks(num_actions, window, input_shape):
     with tf.device("/cpu:0"):
@@ -16,6 +18,31 @@ def build_a3c_networks(num_actions, window, input_shape):
         shared = Convolution2D(name="conv2", nb_filter=32, nb_row=4, nb_col=4, subsample=(2,2), activation='relu', border_mode='same')(shared)
         shared = Flatten()(shared)
         shared = Dense(name="h1", output_dim=256, activation='relu')(shared)
+
+        action_probs = Dense(name="p", output_dim=num_actions, activation='softmax')(shared)
+        
+        state_value = Dense(name="v", output_dim=1, activation='linear')(shared)
+
+        policy_network = Model(input=inputs, output=action_probs)
+        value_network = Model(input=inputs, output=state_value)
+
+        p_params = policy_network.trainable_weights
+        v_params = value_network.trainable_weights
+
+        p_out = policy_network(state)
+        v_out = value_network(state)
+
+    return state, p_out, v_out, p_params, v_params
+
+def build_cartpole_networks(num_actions, window, input_shape):
+    with tf.device("/cpu:0"):
+        state = tf.placeholder("float", [None, window] + list(input_shape))
+
+        inputs = Input(shape=input_shape+(1,))
+        shared = Flatten()(input)
+        shared = Dense(32, activation="relu")(x)
+        shared = Dense(32, activation="relu")(x)
+        shared = Dense(32, activation="relu")(x)
 
         action_probs = Dense(name="p", output_dim=num_actions, activation='softmax')(shared)
         
