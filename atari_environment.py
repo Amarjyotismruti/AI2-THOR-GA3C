5ec2ff023011a9597d03b641b0c72a5d5d11caf9
George Tan <georget1@andrew.cmu.edu>
2017-04-05 06:12:09 -0400

    Move environment to separate file

--- atari_environment.py
+++ atari_environment.py
@@ -3,24 +3,7 @@ from skimage.transform import resize
 from skimage.color import rgb2gray
 import numpy as np
 from collections import deque
-
-class Environment(object):
-    """
-    Small wrapper for environments.
-    Responsible for preprocessing screens and holding on to a screen buffer 
-    of size agent_history_length from which environment state is constructed.
-    """
-    def __init__(self):
-        NotImplementedError
-
-    def get_initial_state(self):
-        NotImplementedError
-
-    def get_preprocessed_frame(self):
-        NotImplementedError
-
-    def step(self):
-        NotImplementedError
+from environment import Environment
 
 class AtariEnvironment(Environment):
     """
