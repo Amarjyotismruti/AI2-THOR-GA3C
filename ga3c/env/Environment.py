0e937f7bfa1a7a55c62339b92fb975bd88c7c85a
George Tan <georget1@andrew.cmu.edu>
2017-04-12 07:43:19 -0400

    Add THOR env

--- ga3c/env/Environment.py
+++ ga3c/env/Environment.py
@@ -1,11 +1,14 @@
 from ga3c.Config import Config
 from ga3c.env.AtariEnvironment import AtariEnvironment
 from ga3c.env.CartPoleEnvironment import CartPoleEnvironment
+from ga3c.env.THOREnvironment import THOREnvironment
 
 def Environment():
-    if 'atari' in Config.NETWORK_NAME:
-        return AtariEnvironment()
-    elif 'cartpole' in Config.NETWORK_NAME:
-        return CartPoleEnvironment()
+	if 'atari' in Config.NETWORK_NAME:
+		return AtariEnvironment()
+	elif 'cartpole' in Config.NETWORK_NAME:
+		return CartPoleEnvironment()
+	elif 'thor' in Config.NETWORK_NAME:
+		return THOREnvironment()
     else:
         raise('Env does not exist.')
